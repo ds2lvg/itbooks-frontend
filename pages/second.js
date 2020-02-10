f1e6e344019ae96be3c5ffa91c08beaed10d481f
HomeReinkaos <ds2lvg@gmail.com>
2020-02-10 19:16:02 +0900

    [front]create-next-app

---
+++ pages/second.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Nav from '../components/nav';
+
+const second = () => {
+  return (
+    <>
+      <Nav></Nav>
+      Second page
+      <button className="btn btn-primary">Button</button>
+    </>
+  );
+};
+
+export default second;
