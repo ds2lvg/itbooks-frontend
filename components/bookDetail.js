f101ee28f539a023cd1a6803ee791dfc05861ed7
HomeReinkaos <ds2lvg@gmail.com>
2020-03-05 14:44:58 +0900

    [도서구매] 1차 구현

--- components/bookDetail.js
+++ components/bookDetail.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from '../utils/axios-config';
 import { useRouter } from 'next/router';
+import { useSelector } from 'react-redux';
 
 const BookDetail = () => {
+  const { isLogin, token, userId } = useSelector(state => state.user);
+  const [ bookId, setBookId ] = useState([]);
   const [ name, setName ] = useState([]);
   const [ price, setPrice ] = useState([]);
   const [ imgSrc, setImgSrc ] = useState([]);
@@ -11,15 +14,29 @@ const BookDetail = () => {
   const getDetail = async () => {
     const url = `/books/detail?bookId=${router.query.bookId}`;
     const result = await axios.get(url);
-    // console.log('BookDetail', result)
+    console.log('BookDetail', result)
 
+    setBookId(result.data.id);
     setName(result.data.name);
     setPrice(result.data.price);
     setImgSrc(result.data.img);
   };
 
-  const buyBook = () => {
+  const buyBook = async () => {
     // 구매 로직 추가할 것
+    const result = await axios.post('/purchase', {
+      uid: userId, bookId, price,
+    });
+    console.log(result);
+
+    // 구매테스트 후 리덕스로 변경
+    if (result.status == 201) {
+      alert('해당 제품 구매를 완료했습니다.')
+    } else if (result.status == 400) {
+      alert('돈이 부족합니다.')
+    } else if (result.status == 401) {
+      alert('구매하시려면 로그인을 해야합니다')
+    }
   }
 
   useEffect(() => {
